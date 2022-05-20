341990fcac4a45f06c8cb7b9838b5484fe5c3a30
Bethel Ngakoutou <ngaradoumbethel@gmail.com>
2022-05-20 09:00:48 +0000

    Début du projet

---
+++ database/migrations/2022_05_19_153335_create_cartes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cartes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('matricule');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('poste');
+            $table->string('titre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cartes');
+    }
+}
